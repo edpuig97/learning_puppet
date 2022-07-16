671fd380bc3c4378baebb5c0c5023cda272df237
Eduardo Puig <edpuig97@gmail.com>
2022-07-16 10:00:56 -0400

    Fixing tipo (-manifest +manifests/)

--- files/run-puppet.sh
+++ files/run-puppet.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /etc/puppetlabs/code/environments/production && git pull
-/opt/puppetlabs/bin/puppet apply manifest
+/opt/puppetlabs/bin/puppet apply manifests/
 var=$(date)
 echo "[$var] Puppet was here... ;)">> /tmp/puppet-was-here
