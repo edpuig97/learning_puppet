bf9646e153911a04f06080768a37c7c4a7d9ff24
Eduardo Puig <edpuig97@gmail.com>
2022-07-16 09:56:23 -0400

    Add method to verify puppet is running manifests automatically

--- files/run-puppet.sh
+++ files/run-puppet.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /etc/puppetlabs/code/environments/production && git pull
-/opt/puppetlabs/bin/puppet apply manifests/
+/opt/puppetlabs/bin/puppet apply manifest
+echo "[$(date)] Puppet was here... ;)">> /tmp/puppet-was-here
